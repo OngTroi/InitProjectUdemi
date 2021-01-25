e25321015100aa1d7185a461dca48c00e4aa036e
ThienTran <sky.endless96@gmail.com>
2021-01-25 21:20:04 +0700

    ReInit Project

---
+++ Udemi.Api/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Udemi.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Udemi.Services/UnitOfWork.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemi.DBConnection.Interfaces;
+using Udemi.DBConnection.Repository;
+
+namespace Udemi.Services
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        #region Initalize
+        private string _connectionString;
+
+        private bool _disposed;
+
+        public UnitOfWork(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public ITransaction BeginTransaction()
+        {
+            return new Transaction();
+        }
+
+        public void EndTransaction(ITransaction transaction)
+        {
+            if (transaction != null)
+            {
+                transaction.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // new DatabaseServiceSql(_connectionString).TryDispose();
+                }
+            }
+            _disposed = true;
+        }
+        #endregion
+
+    }
+}
